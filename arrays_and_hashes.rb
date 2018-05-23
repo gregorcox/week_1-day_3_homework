222e1865b4f93b810ebf8e313a924e430c810388
Gregor Cox <gregorcox@gmail.com>
2018-05-23 17:32:43 +0100

    completed exercise B

--- arrays_and_hashes.rb
+++ arrays_and_hashes.rb
@@ -104,12 +104,35 @@ p users["Erik"][:home_town]
 #3. Get the array of Erik's lottery numbers
 p users["Erik"][:lottery_numbers]
 #4. Get the type of Avril's pet Monty
+p users["Avril"][:pets][0][:species]
 #5. Get the smallest of Erik's lottery numbers
+p users["Erik"][:lottery_numbers].min
 #6. Return an array of Avril's lottery numbers that are even
+def return_even(array)
+  for number in array
+    if number % 2 == 0
+      p number
+    end
+  end
+end
+
+return_even(users["Avril"][:lottery_numbers])
+
+
 #7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+p users["Erik"][:lottery_numbers].push(7)
 #8. Change Erik's hometown to Edinburgh
+p users["Erik"][:home_town] = "Edinburgh"
+
 #9. Add a pet dog to Erik called "Fluffy"
+users["Erik"][:pets] << {:name => "Fluffy", :species => "dog"}
+
+p users["Erik"][:pets]
+
 #10. Add another person to the users hash
+users["Steve"] = {}
+
+p users
 
 
 # Exercise C
